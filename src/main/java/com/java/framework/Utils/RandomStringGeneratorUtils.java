467a1683c0a78ff4666f39afd0f84dea4d25ef13
dheerajps77 <dheerajps77@gmail.com>
2019-06-12 12:20:30 +0530

    doc, RandomStringUtils

---
+++ src/main/java/com/java/framework/Utils/RandomStringGeneratorUtils.java
@@ -0,0 +1,26 @@
+package com.java.framework.Utils;
+
+public class RandomStringGeneratorUtils {
+	
+	
+	public static String RandomString()
+	{
+		String stringValue="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz";
+		StringBuffer stringbuffer = null;
+		try {
+			
+			stringbuffer=new StringBuffer(10);
+			
+			for(int i=0;i<10;i++)
+			{
+				int index=(int)(stringValue.length()*Math.random());
+				stringbuffer.append(stringValue.charAt(index));
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return stringbuffer.toString()+"@gmail.com";
+	}
+
+}
--- src/test/java/RoughWork/PieChartInSelenium.java
+++ src/test/java/RoughWork/PieChartInSelenium.java
@@ -1,13 +1,17 @@
 package RoughWork;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+
 
 public class PieChartInSelenium {
 
@@ -51,24 +55,73 @@ public class PieChartInSelenium {
 		}
 	}
 
-	public static void PieChart(WebDriver driver, Actions actions) {
+	public static ArrayList<String> PieChart(WebDriver driver, Actions actions) {
+		
+		ArrayList<String> arryList=new ArrayList<String>();
 		try {
 
 			List<WebElement> colors=driver.findElements(By.xpath("//*[contains(@class,'yui3-svgSvgPieSlice')]"));
 			WebElement ToolTip = driver.findElement(By.xpath("//div[contains(@id,'_tooltip')]"));
 
-			int totalCountOfColorsInPieChart=colors.size();
-			System.out.println("-X-X-X-X-X-X-X-X- Colors present in PieChart -X-X-X-X-X-X-X-X-");
-			
+			int totalCountOfColorsInPieChart=colors.size();	
+			System.out.println("Total blocks of pie chart are as : --> ");
 			for(int i=1;i<=totalCountOfColorsInPieChart;i++)
 			{
 				WebElement colorElement=driver.findElement(By.xpath("//*[contains(@class,'yui3-svgSvgPieSlice')]["+i+"]"));
 				
 				actions=new Actions(driver);
 				actions.moveToElement(colorElement).build().perform();
-				String colorsStringValue=ToolTip.getText();
-				System.out.println(colorsStringValue);
+				String colorsStringValue=ToolTip.getText();				
+				arryList.add(colorsStringValue);
 			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return arryList;
+	}
+	
+	public static void PieChartReader(WebDriver driver, Actions action)
+	{
+		try {
+			
+			ArrayList<String> arrayList=PieChart(driver, action);		
+			Iterator<String> iterator=arrayList.iterator();
+			System.out.println("-X-X-X-X-X-X-X-X- Colors present in PieChart -X-X-X-X-X-X-X-X-");
+			while(iterator.hasNext())
+			{
+				String value=iterator.next();
+				System.out.println(value);
+			}
+			
+		} catch (Exception e) {
+			
+		}
+	}
+	
+	
+	public static void innerTextHTMLModification(WebElement element , WebDriver driver)
+	{
+		try {
+			JavascriptExecutor js=(JavascriptExecutor)driver;
+			js.executeScript("var ele=arguments[0]; ele.innerHTML = 'Hu';", element);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void PieChartModification(WebDriver driver, Actions action)
+	{
+		try {
+			
+			driver.findElement(By.xpath("//*[@id='gc-wrapper']/div[2]/article/article/div[2]/form[1]/input[1]")).click();
+			WebElement runButton=driver.findElement(By.xpath("//a[@class='aiButton' and @id='run']"));			
+			WebElement blueColor=driver.findElement(By.xpath("//*[@id='editor']/div[1]/div[3]/div[2]/div[6]/div[1]/div/div/div/div[5]/div[18]/pre/span/span[1]"));	
+			innerTextHTMLModification(blueColor, driver);
+			Thread.sleep(2000);
+			runButton.click();			
+			driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@name='result']")));
+			Thread.sleep(2000);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -77,18 +130,19 @@ public class PieChartInSelenium {
 	public static void main(String[] args) throws InterruptedException {
 
 		WebDriver driver;
-		String pieURL = "https://yuilibrary.com/yui/docs/charts/charts-pie.html";
+		String pieChartURL = "https://yuilibrary.com/yui/docs/charts/charts-pie.html";
 		String barURL = "https://yuilibrary.com/yui/docs/charts/charts-dualaxes.html";
-
+		String pieChartURL1="https://developers.google.com/chart/interactive/docs/basic_multiple_charts";
 		System.setProperty("webdriver.chrome.driver",
 				"C:\\Users\\dheeraj.singh\\git\\JavaFrameWork\\Drivers\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.get(pieURL);
+		driver.get(pieChartURL1);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(4000, TimeUnit.SECONDS);
 		driver.manage().timeouts().pageLoadTimeout(4000, TimeUnit.SECONDS);		
 		Actions actions = null;
-		 PieChart(driver, actions);
 		//BarGraph(driver, actions);
+		//PieChartReader(driver, actions);
+		PieChartModification(driver, actions);
 	}
 }
